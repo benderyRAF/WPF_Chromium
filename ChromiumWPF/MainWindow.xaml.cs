6a479558062f39bf0c79ac758e68dbc5d26345be
ShonDimovRAF <SHONDIM@rafael.co.il>
2022-06-13 15:38:17 +0300

    Implemented auto check for online/offline states.

--- ChromiumWPF/MainWindow.xaml.cs
+++ ChromiumWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CefSharp;
 using CefSharp.Wpf;
+using System.Runtime.InteropServices;
+using System.Windows.Threading;
 
 namespace ChromiumWPF {
     /// <summary>
@@ -21,12 +24,72 @@ namespace ChromiumWPF {
     /// </summary>
     public partial class MainWindow : Window {
 
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int connectionDescription, int reservedValue);
+
         private int tabCount = 0;
         private TabItem currentTabItem = null;
         private ChromiumWebBrowser currentBrowserShowing = null;
 
+        private struct Page {
+            public string header { get; set; }
+            public string address { get; set; }
+        }
+
+        private readonly Dictionary<bool, Page> PAGES = new Dictionary<bool, Page>() {
+            { false, new Page {
+                header = "off page",
+                address = "file:///C:/Users/SHONDIM/Desktop/LocalhostOff.html",
+            }},
+            { true, new Page{
+                header = "on page",
+                address = "file:///C:/Users/SHONDIM/Desktop/LocalhostOn.html",
+            }},
+        };
+
+        private bool connected = true;
+
         public MainWindow() {
+
             InitializeComponent();
+            connected = InternetGetConnectedState(out _, 0);
+
+            TabItem tabItem = new TabItem {
+                Name = "defaultPage",
+                Header = PAGES[connected].header
+            };
+            tabControl.Items.Add(tabItem);
+
+            ChromiumWebBrowser browser = new ChromiumWebBrowser(PAGES[connected].address) {
+                Name = "defaultBrowser",
+                BrowserSettings = {
+                    DefaultEncoding = "UTF-8",
+                    WebGl = CefState.Disabled
+                }
+            };
+
+            tabItem.Content = browser;
+
+            currentTabItem = tabItem;
+            currentBrowserShowing = browser;
+
+            // Checks internet connection every second.
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(checkConnection);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+
+        }
+
+        private void checkConnection(object sender, EventArgs e) {
+
+            bool check = InternetGetConnectedState(out _, 0);
+            if (check == connected) return;
+
+            // Refreshing & changing connection state.
+            connected = check;
+            currentTabItem.Header = PAGES[connected].header;
+            currentBrowserShowing.Address = PAGES[connected].address;
 
         }
 
@@ -59,7 +122,6 @@ namespace ChromiumWPF {
         private void FinishLoadingWebPage(object sender, RoutedEventArgs e) {
 
             var sndr = sender as ChromiumWebBrowser;
-
             currentTabItem.Header = sndr.Address;
 
         }
@@ -104,5 +166,6 @@ namespace ChromiumWPF {
             SettingsWindow setting = new SettingsWindow();
             setting.ShowDialog();
         }
+
     }
 }
